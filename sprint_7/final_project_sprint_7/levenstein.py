d78029c6346a5aa121b4262e1f51f55101c88fd8
VladMerk <merkvladimir@gmail.com>
2024-06-07 05:25:30 +0300

    первая финальная задача  спринта 7: Расстояние по Левенштейну

---
+++ sprint_7/final_project_sprint_7/levenstein.py
@@ -0,0 +1,22 @@
+def levenshtein_distance(s, t):
+    m, n = len(s), len(t)
+    dp: list[list[int]] = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(m + 1):
+        dp[i][0] = i
+    for j in range(n + 1):
+        dp[0][j] = j
+
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if s[i - 1] == t[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+
+    return dp[m][n]
+
+
+s: str = input().strip()
+t: str = input().strip()
+print(levenshtein_distance(s, t))
